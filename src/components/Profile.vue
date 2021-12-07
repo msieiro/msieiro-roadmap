26807563b265621dc836a249b0c821abc6fcadcd
Martin Sieiro <martinsieiro@institutointerculturas.com>
2021-12-07 12:30:16 +0100

    fix profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -50,14 +50,6 @@
           >
           <v-card-text class="white text--primary pt-2">
             <em> Downloader and compressor of private videos from Vimeo. </em>
-            <!-- <img
-              v-for="(tech, t) in learning"
-              :key="t"
-              :src="technologies[tech] && technologies[tech].src"
-              :alt="technologies[tech] && technologies[tech].name"
-              height="45"
-              class="mx-1 my-1"
-            /> -->
             <v-card-actions>
               <v-btn
                 small
@@ -67,7 +59,7 @@
                 target="_blank"
                 href="https://github.com/msieiro/vimeo-downloader-compressor"
               >
-                Ver repositorio
+                {{ $t("general.watchrepository") }}
               </v-btn>
             </v-card-actions>
           </v-card-text>
