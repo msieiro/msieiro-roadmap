59a8d713c1999812c31ac1cc9c5e235609c76348
Martin Sieiro <martinsieiro@institutointerculturas.com>
2021-12-07 16:15:31 +0100

    changed profile chips colors and refactor

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -3,14 +3,14 @@
     <v-card elevation="2" outlined max-width="800" class="mx-auto">
       <v-row>
         <v-col sm="12" md="4">
-          <v-img rounded src="../assets/images/yo.jpeg"></v-img>
+          <v-img src="../assets/images/yo.jpeg"></v-img>
         </v-col>
         <v-col sm="12" md="8">
           <v-card-title
             class="text-h6 text-center mx-auto d-flex justify-sm-space-between justify-center morePadding minimizedText"
             ><v-chip
               class="ma-2 mx-auto ml-1 mr-1 minimizedText"
-              color="blue darken-2"
+              color="green darken-3"
               text-color="white"
             >
               A Coruña
@@ -18,7 +18,7 @@
             ><span class="minimizedText">Martín Sieiro Gutiérrez</span
             ><v-chip
               class="ma-2 mx-auto ml-1 mr-1 minimizedText"
-              color="pink darken-2"
+              color="cyan darken-3"
               text-color="white"
             >
               26 {{ $t("profile.years") }}
@@ -31,7 +31,7 @@
           </v-card-text>
           <v-card-actions class="d-flex flex-wrap justify-center">
             <img
-              v-for="(tech, t) in learning"
+              v-for="(tech, t) in profile.learning"
               :key="t"
               :src="technologies[tech] && technologies[tech].src"
               :alt="technologies[tech] && technologies[tech].name"
@@ -42,42 +42,21 @@
         </v-col>
       </v-row>
     </v-card>
-    <v-timeline align-top :dense="$vuetify.breakpoint.smAndDown">
-      <v-timeline-item icon="mdi-github" color="black">
-        <v-card dark class="text-justify" color="black" max-width="300">
-          <v-card-title class="text-subtitle-1">
-            vimeo-downloader-compressor</v-card-title
-          >
-          <v-card-text class="white text--primary pt-2">
-            <em> Downloader and compressor of private videos from Vimeo. </em>
-            <v-card-actions>
-              <v-btn
-                small
-                text
-                color="black"
-                class="mx-auto"
-                target="_blank"
-                href="https://github.com/msieiro/vimeo-downloader-compressor"
-              >
-                {{ $t("general.watchrepository") }}
-              </v-btn>
-            </v-card-actions>
-          </v-card-text>
-        </v-card>
-      </v-timeline-item>
-    </v-timeline>
+    <Repository :repository="profile.repository" />
   </v-container>
 </template>
 
 <script>
 import technologies from "../data/technologies";
-import learning from "../data/learning";
+import profile from "../data/profile";
+import Repository from "./Repository";
 export default {
   name: "Profile",
+  components: { Repository },
   data() {
     return {
       technologies,
-      learning,
+      profile,
     };
   },
 };
