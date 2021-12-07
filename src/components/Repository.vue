794bad216a5a461f778b30eac8a6f576301eef28
Martin Sieiro <martinsieiro@institutointerculturas.com>
2021-12-07 16:15:42 +0100

    created repository component

---
+++ src/components/Repository.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-timeline align-top :dense="$vuetify.breakpoint.smAndDown">
+    <v-timeline-item icon="mdi-github" color="black">
+      <v-card dark class="text-justify" color="black" max-width="300">
+        <v-card-title class="text-subtitle-1">
+          {{ repository.title }}</v-card-title
+        >
+        <v-card-text class="white text--primary pt-2">
+          <em>{{ repository.description }} </em>
+          <v-card-actions>
+            <v-btn
+              small
+              text
+              color="black"
+              class="mx-auto"
+              target="_blank"
+              :href="repository.src"
+            >
+              {{ $t("general.gotorepository") }}
+            </v-btn>
+          </v-card-actions>
+        </v-card-text>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
+</template>
+
+<script>
+export default {
+  name: "Repository",
+  props: {
+    repository: Object,
+  },
+};
+</script>
+
+<style></style>
