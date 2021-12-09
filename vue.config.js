74e495c622f8609b82ba34cc536138aac91834e2
Martin Sieiro <martinsieiro@institutointerculturas.com>
2021-12-09 14:36:35 +0100

    js chunks optimization

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-
+  configureWebpack: {
+    performance: {
+      hints: false,
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      },
+    },
+  },
   pluginOptions: {
     i18n: {
       locale: "es",
