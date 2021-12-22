d133c219f0f358fa0b44cd5bff4546cb6e6e77e8
Martin Sieiro <martinsieiro@institutointerculturas.com>
2021-12-22 23:00:39 +0100

    performance improvement

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,8 +1,59 @@
 import "@mdi/font/css/materialdesignicons.min.css";
 import Vue from "vue";
-import "vuetify/dist/vuetify.min.css";
-import Vuetify from "vuetify/dist/vuetify.min.js";
+import Vuetify, {
+  VApp,
+  VAppBar,
+  VBtn,
+  VCard,
+  VCardActions,
+  VCardText,
+  VCardTitle,
+  VChip,
+  VCol,
+  VContainer,
+  VDivider,
+  VFooter,
+  VIcon,
+  VImg,
+  VList,
+  VListItemTitle,
+  VMain,
+  VMenu,
+  VRow,
+  VSpacer,
+  VTimeline,
+  VTimelineItem,
+  VToolbarTitle,
+} from "vuetify/lib";
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VMain,
+    VCard,
+    VFooter,
+    VCol,
+    VMenu,
+    VBtn,
+    VList,
+    VListItemTitle,
+    VDivider,
+    VAppBar,
+    VToolbarTitle,
+    VSpacer,
+    VIcon,
+    VRow,
+    VContainer,
+    VTimeline,
+    VTimelineItem,
+    VCardTitle,
+    VCardText,
+    VImg,
+    VChip,
+    VCardActions,
+  },
+});
 
-export default new Vuetify({});
+const opts = {};
+
+export default new Vuetify(opts);
