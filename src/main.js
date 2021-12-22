c00139374ccf6b6ee5b194eb9c34a0715462e4c7
Martin Sieiro <martinsieiro@institutointerculturas.com>
2021-12-22 22:43:21 +0100

    update vuetify plugin source

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import App from "@/App.vue";
 import i18n from "@/plugins/i18n";
-import vuetify from "@/plugins/vuetify"; // path to vuetify export
+import vuetify from "@/plugins/vuetify";
 import Vue from "vue";
 import "./sass/main.scss";
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,7 @@
 import "@mdi/font/css/materialdesignicons.min.css";
 import Vue from "vue";
-import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
+import Vuetify from "vuetify/dist/vuetify.min.js";
 
 Vue.use(Vuetify);
 
